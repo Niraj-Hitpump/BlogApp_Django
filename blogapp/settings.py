0e96fa0302a61ec200d6164bec2892f6c8296c43
Niraj Kumar <niraj@example.com>
2024-08-10 15:32:21 +0530

    updated

--- blogapp/settings.py
+++ blogapp/settings.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 import os
-from decouple import config
+# from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,5 +132,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = config('EMAIL')
-EMAIL_HOST_PASSWORD = config('PASSWORD')
+# EMAIL_HOST_USER = config('EMAIL')
+# EMAIL_HOST_PASSWORD = config('PASSWORD')
